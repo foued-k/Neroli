3dc56a00445980e4e3d353e28db306cf450e2dbd
Foued <fouad.kabaou@gmail.com>
2024-02-08 22:13:44 +0100

    database

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
 
 //configure env
 dotenv.config();
 
+//database config
+connectDB();
+
 const app = express();
+
+//middelwares
+app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send({ message: "welcome to Neroli mern app" });
