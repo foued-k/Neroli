d1d206bd7f73e10465292626eda929e101c102df
Foued <fouad.kabaou@gmail.com>
2024-02-09 20:58:16 +0100

    hashing password

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 //configure env
 dotenv.config();
@@ -11,9 +12,12 @@ connectDB();
 
 const app = express();
 
-//middelwares
+//middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send({ message: "welcome to Neroli mern app" });
