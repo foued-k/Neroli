091ea7c372757858017fd55016cd5bf84f104c63
Foued <fouad.kabaou@gmail.com>
2024-02-12 21:09:32 +0100

    starting with react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>NEROLI APP</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
