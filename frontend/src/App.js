52fb7587edacffeeb24eb7a684d2cbd1861f2024
Foued <fouad.kabaou@gmail.com>
2024-02-14 22:28:38 +0100

    homepage routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
-import "./App.css";
-import Layout from "./components/Layout/Layout";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import Pagenotfound from "./pages/Pagenotfound";
 
 function App() {
   return (
     <>
-      <Layout>
-        <h1>NEROLI APP</h1>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="*" element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className="bg-dark text-light p-3">
+    <div className="footer">
       <h4 className="text-center">All Right reserved &copy; Neroli </h4>
+      <p className="text-center mt-3"></p>
+      <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
+      <Link to="/policy">Privacy Policy</Link>
     </div>
   );
 };
