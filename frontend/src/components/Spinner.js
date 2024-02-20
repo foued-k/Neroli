f5552c0f7386866804ac3788c8804cad3fab54be
Foued <fouad.kabaou@gmail.com>
2024-02-20 01:21:01 +0100

    updates2

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Spinner = () => {
+  const [count, setCount] = useState(5);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((preValue) => --preValue);
+    }, 1000);
+    count === 0 && navigate("/login");
+    return () => clearInterval(interval);
+  }, [count, navigate]);
   return (
     <>
       <div
-        className="d-flex justify-content-center align-items-center"
+        className="d-flex flex-column justify-content-center align-items-center"
         style={{ height: "100vh" }}
       >
+        <h1 className="text-center">redirecting to you in {count} seconds </h1>
         <div className="spinner-border" role="status">
           <span className="visually-hidden">Loading...</span>
         </div>
